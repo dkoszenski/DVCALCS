c093e9aa679fd82d5c0311cff552500d6f367e72
dkosz <dkosz@DK-HOME-PC>
2023-10-31 07:13:28 -0400

    Add project files.

---
+++ DVCALCS/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DVCALCS
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ DVCALCS/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DVCALCS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
